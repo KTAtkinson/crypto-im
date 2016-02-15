182db5bb41d2c2d20bac59cf43ba27b59597e8f0
Katie Atkinson <kaitlin@atkinson.mn>
2016-02-15 15:12:23 -0800

    Implemented error message showing/hiding in javascript.

--- static/chat-page.js
+++ static/chat-page.js
@@ -1,8 +1,23 @@
+function beforeFormSubmit() {
+    var errorContainer = $(form).find('error');
+    errorContainer.hide();
+    errorContainer.text('');
+}
+
+
+function addError(resp) {
+    var messageContainer = $(form).find('.error');
+    messageContainer.text(resp.responseJSON.error);
+    messageContainer.show();
+}
+
+
 function joinChat(evt) {
     evt.preventDefault();
+    window.form = evt.target;
 
     var request = {
-        name: $('form input[name="name"').val(),
+        name: $('form input[name="name"]').val(),
         // Adding public key as empty string for now.
         public_key: ''
         };
@@ -12,9 +27,11 @@ function joinChat(evt) {
         'data': request,
         'method': 'POST'
         })
+        .always(beforeFormSubmit)
         .success(function(data) {
             window.user_id = data.new_user_id;
-            });
+            })
+        .fail(addError);
 }
 
 
